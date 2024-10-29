26c1a4776c46f928215365706a62aa76951fd742
sidneyferracinjr <sidneyferracinjr@gmail.com>
2024-10-29 16:21:49 -0300

    feat: :lipstick: start the front end using angular

--- backend/pegho/models.py
+++ backend/pegho/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 
 class DadosPessoais(models.Model):
-    nome = models.CharField(max_length=100)
-    data_nascimento = models.DateField()
+    nome = models.CharField(max_length=100, blank=False, null=False)
+    cpf = models.CharField(max_length=14, blank=False, null=False, default='000.000.000-00')
+    data_nascimento = models.DateField(blank=False, null=False)
 
 class Contato(models.Model):
-    email = models.EmailField()
-    telefone = models.CharField(max_length=20)
-    endereco = models.CharField(max_length=255)
+    email = models.EmailField(blank=False, null=False)
+    telefone = models.CharField(max_length=20, blank=False, null=False)
+    endereco = models.CharField(max_length=255, blank=False, null=False)
 
 class ExperienciaProfissional(models.Model):
     cargo = models.CharField(max_length=100)
